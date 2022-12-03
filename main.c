974769a4490a82fedb381f94bdd86f95ff37013b
leflanagan <66319333+leflanagan@users.noreply.github.com>
2022-12-03 15:09:37 -0500

    no errors

--- main.c
+++ main.c
@@ -6,77 +6,82 @@
 #include <string.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 // Set the following port to a unique number:
 #define MYPORT 5950
 
-int create_cs3516_socket() {
-    int sock;
-    struct sockaddr_in server;
-    
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
+int create_cs3516_socket()
+{
+  int sock;
+  struct sockaddr_in server;
 
-    if (sock < 0) perror("Error creating CS3516 socket");
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    bzero(&server, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(MYPORT);
-    if (bind(sock, (struct sockaddr *) &server, sizeof(server) ) < 0) 
-        perror("Unable to bind CS3516 socket");
+  if (sock < 0)
+    perror("Error creating CS3516 socket");
 
-    // Socket is now bound:
-    return sock;
+  bzero(&server, sizeof(server));
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  server.sin_port = htons(MYPORT);
+  if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    perror("Unable to bind CS3516 socket");
+
+  // Socket is now bound:
+  return sock;
 }
 
-int cs3516_recv(int sock, char *buffer, int buff_size) {
-    struct sockaddr_in from;
-    int fromlen, n;
-    fromlen = sizeof(struct sockaddr_in);
-    n = recvfrom(sock, buffer, buff_size, 0,
-                 (struct sockaddr *) &from, &fromlen);
+int cs3516_recv(int sock, char *buffer, int buff_size)
+{
+  struct sockaddr_in from;
+  int fromlen, n;
+  fromlen = sizeof(struct sockaddr_in);
+  n = recvfrom(sock, buffer, buff_size, 0,
+               (struct sockaddr *)&from, &fromlen);
 
-    return n;
+  return n;
 }
 
-int cs3516_send(int sock, char *buffer, int buff_size, unsigned long nextIP) {
-    struct sockaddr_in to;
-    int tolen, n;
+int cs3516_send(int sock, char *buffer, int buff_size, unsigned long nextIP)
+{
+  struct sockaddr_in to;
+  int tolen, n;
 
-    tolen = sizeof(struct sockaddr_in);
+  tolen = sizeof(struct sockaddr_in);
 
-    // Okay, we must populate the to structure. 
-    bzero(&to, sizeof(to));
-    to.sin_family = AF_INET;
-    to.sin_port = htons(MYPORT);
-    to.sin_addr.s_addr = nextIP;
+  // Okay, we must populate the to structure.
+  bzero(&to, sizeof(to));
+  to.sin_family = AF_INET;
+  to.sin_port = htons(MYPORT);
+  to.sin_addr.s_addr = nextIP;
 
-    // We can now send to this destination:
-    n = sendto(sock, buffer, buff_size, 0,
-               (struct sockaddr *)&to, tolen);
+  // We can now send to this destination:
+  n = sendto(sock, buffer, buff_size, 0,
+             (struct sockaddr *)&to, tolen);
 
-    return n;
+  return n;
 }
 
-int main(int argc, char *argv[]) {
-    int host_flag = 0;
-    int router_flag = 0;
+int main(int argc, char *argv[])
+{
+  int host_flag = 0;
+  int router_flag = 0;
 
-    int opterr = 0;
-    int opt;
+  int opterr = 0;
+  int opt;
 
-  while ((opt = getopt (argc, argv, "hr:")) != -1)
+  while ((opt = getopt(argc, argv, "hr:")) != -1)
     switch (opt)
-      {
-      case 'h':
-        host_flag = 1;
-        break;
-      case 'r':
-        router_flag = 1;
-        break;
-      default:
-        abort ();
-      }
-
-
+    {
+    case 'h':
+      host_flag = 1;
+      break;
+    case 'r':
+      router_flag = 1;
+      break;
+    default:
+      abort();
+    }
 }
